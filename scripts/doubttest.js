863257ab98c497573fb9bfe728b6764418981574
shruthi093 <jayasruthi.l@gmail.com>
2022-04-18 13:46:49 +0530

    git commands

---
+++ scripts/doubttest.js
@@ -0,0 +1,39 @@
+/*var strings=["react","react","js","js"];
+const unique = new Map();
+for(var i=0;i<strings.length;i++){
+   if(!unique.has(strings[i]))
+    unique.set(strings[i]);
+}
+
+var arr=new Array();
+for (var x of unique.keys()) {
+    arr.push(x);
+  }
+  for(var i=0;i<=arr.length;i++){
+      console.log(arr[i]);
+  }
+  let strings=["react","react","js","js"];
+let res=strings.filter((item)=>item==="react")
+console.log(res.length)
+let rupee="$450"
+console.log(450===Number(rupee.slice(1,4)))
+
+setTimeout(()=>{
+  console.log("function is execites")
+},3000)
+function positive(no){
+  return no>=0;
+}
+function result(nos){
+  let data=[]
+  for(let i=0;i<nos.length;i++){
+    if(positive(nos[i])){
+      data.push(nos[i])
+    }
+  }
+  //loc
+  return data
+}
+const response=result([2,-1,-2,-3],positive)
+console.log(response)*/
+
